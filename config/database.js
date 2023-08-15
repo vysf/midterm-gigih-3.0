77ec10a6cea4a6f545ca970d1b8d588c9b2dd102
vysf <yusufumar@student.untan.ac.id>
2023-08-15 21:41:23 +0700

    add mongodb atlas url

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 module.exports = {
   mongodb: {
-    uri: 'mongodb://127.0.0.1:27017/midterm-gigih',
+    uri: `${process.env.MONGODB_URI}/midterm-gigih` || 'mongodb://127.0.0.1:27017/midterm-gigih',
     options: {
       useNewUrlParser: true,
       useUnifiedTopology: true,
